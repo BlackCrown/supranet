d125d3a42474982e27970767af4ca1e3dcdc399d
Wagner Araujo <wagnercorp17@gmail.com>
2025-01-20 20:51:03 -0300

    Header criado, logo adcionada, nav adcionado com links para navegacao estilizados

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,8 +1,28 @@
 import React from 'react';
 import styles from './Header.module.css';
+import logo from '../assets/SupraNet_P.png';
 
 const Header = () => {
-  return <div className={styles.header}>Header</div>;
+  return (
+    <div className={styles.header}>
+      <nav className={styles.nav}>
+        <div className={styles.logoImg}>
+          <img src={logo} alt="Logo da empresa SupraNet" />
+        </div>
+        <ul>
+          <li>
+            <a href="#">empresa</a>
+          </li>
+          <li>
+            <a href="#planos">planos</a>
+          </li>
+          <li className={styles.btnContratar}>
+            <a href="#contratar">contratar</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default Header;
