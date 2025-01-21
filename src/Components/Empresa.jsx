6a2c7ab2bb35a7507e0e02604d9910e4a93940f9
Wagner Araujo <wagnercorp17@gmail.com>
2025-01-20 22:26:18 -0300

    cabecalho finalizado, iniciando a criacao do corpo da pagina. Secao de Planos

---
+++ src/Components/Empresa.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './Empresa.module.css';
+
+const Empresa = () => {
+  return <section className={styles.empresa}>Empresa</section>;
+};
+
+export default Empresa;
---
+++ src/Components/Planos.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import styles from './Planos.module.css';
+
+const Planos = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.cabecalho}>
+        <p>
+          Planos sobre medida
+          <br /> Feitos para <strong>você</strong>
+          <br /> ou <br />
+          <strong>Sua empresa!!!</strong>
+        </p>
+        <p>
+          Venha fazer parte dos nossos milhares de
+          <br />
+          <strong>clientes satisfeitos!!!</strong>
+        </p>
+      </div>
+      <div className="conheca">
+        <h1>Conheça nossos planos</h1>
+        <div>
+          <h3>PLANOS COMERCIAIS A PARTIR DE R$80 MENSAIS</h3>
+          <ul>
+            <li>Roteador de ultima geração</li>
+            <li>Instalação em até 24 hrs</li>
+            <li>Primeira fatura após 30 dias</li>
+            <li>Suporte 24hrs por dia de seg à sexta</li>
+          </ul>
+        </div>
+        <div>
+          <h3>PLANOS EMPRESARIAIS PERSONALIZADOS</h3>
+          <ul>
+            <li>Até 3 roteadores sem custo adicional</li>
+            <li>Consultoria especializada em TI e Redes</li>
+            <li>Planos anuais com desconto</li>
+            <li>Suporte 24hrs todos os dias</li>
+          </ul>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Planos;
