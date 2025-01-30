ff2b90959376e3aa5cac1f5686089f12bd73a01e
Wagner Araujo <wagnercorp17@gmail.com>
2025-01-30 20:31:24 -0300

    Adicionados novos assets (imgs) textes estilizados e posicionados inicialmentes, verificar alinhamento e espacamentos dos textos de acordo com a pagina geral, adcionada img de background da section comercial

--- src/Components/Empresa.jsx
+++ src/Components/Empresa.jsx
@@ -5,28 +5,31 @@ const Empresa = () => {
   return (
     <article className={styles.SobreEmpresa}>
       <h1>Sobre a nossa Empresa</h1>
-      <section className={styles.comercial}>
-        <p>
-          <strong>SupraNet</strong> é uma <strong>empresa</strong> de internet
-          dedicada a fornecer soluções de conectividade de
-          <strong>alta qualidade</strong> para o mercado comercial e
-          empresarial.
-        </p>
-        <p>
-          Com um enfoque em planos acessíveis e personalizados, a
-          <strong>SupraNet</strong> está comprometida em oferecer serviços que
-          atendam às necessidades de ,<strong>empresas</strong> de todos os
-          tamanhos, desde startups até grandes corporações.
-        </p>
-      </section>
-      <section className={styles.empresarial}>
-        <p>
-          Nossa missão é <strong>conectar negócios</strong> ao mundo digital de
-          maneira eficaz e econômica, proporcionando uma experiência de
-          <strong>internet superior</strong> que impulsiona a
-          <strong>produtividade</strong> e a <strong>inovação!!!</strong>
-        </p>
-      </section>
+      <div className={styles.corpo}>
+        <section className={styles.comercial}>
+          <span className={styles.bgImgComercial} />
+          <p className={styles.txt1}>
+            <strong>SupraNet</strong> é uma <strong>empresa</strong> de internet
+            dedicada a fornecer soluções de conectividade de
+            <strong>alta qualidade</strong> para o mercado comercial e
+            empresarial.
+          </p>
+          <p className={styles.txt2}>
+            Com um enfoque em planos acessíveis e personalizados, a
+            <strong>SupraNet</strong> está comprometida em oferecer serviços que
+            atendam às necessidades de ,<strong>empresas</strong> de todos os
+            tamanhos, desde startups até grandes corporações.
+          </p>
+        </section>
+        <section className={styles.empresarial}>
+          <p>
+            Nossa missão é <strong>conectar negócios</strong> ao mundo digital
+            de maneira eficaz e econômica, proporcionando uma experiência de
+            <strong>internet superior</strong> que impulsiona a
+            <strong>produtividade</strong> e a <strong>inovação!!!</strong>
+          </p>
+        </section>
+      </div>
     </article>
   );
 };
