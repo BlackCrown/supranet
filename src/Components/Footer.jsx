d38bfca23603b1375d82984ffb87b8105f4bcbce
Wagner Araujo <wagnercorp17@gmail.com>
2025-01-31 21:31:31 -0300

    html criado, iniciando criacao de modulo para formulario

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,45 @@ import React from 'react';
 import styles from './Footer.module.css';
 
 const Footer = () => {
-  return <div className={styles.main}>Footer</div>;
+  return (
+    <div className={styles.main}>
+      <div>
+        <h1 className="Titulo">Menu</h1>
+        <nav>
+          <ul>
+            <li>
+              <a href="#">Quero contratar</a>
+            </li>
+            <li>
+              <a href="#">Planos para Empresaas</a>
+            </li>
+            <li>
+              <a href="#">Trabalhe Conosco</a>
+            </li>
+            <li>
+              <a href="#">Sobre a SupraNet</a>
+            </li>
+            <li>
+              <a href="#">Suporte on-line</a>
+            </li>
+            <li>
+              <a href="#">Duvidas frequentes</a>
+            </li>
+            <li>
+              <a href="#">Meios de contato</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+
+      <div>
+        <h1 className="Titulo">Solicite Contato:</h1>
+        <div>
+          <form action=""></form>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
